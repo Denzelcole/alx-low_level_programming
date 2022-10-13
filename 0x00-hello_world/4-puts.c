af8f71ca9773a49e571cd9fed98ff87f669a229a
Denzelcole <theocole4@gmail.com>
2022-10-13 12:51:13 -0700

    Added comments for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+* main - prints the function with puts
+*
+* Description:
+* This program prints Programming is like a building multilingual puzzle
+*
+* Return: 0 (means succes)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
