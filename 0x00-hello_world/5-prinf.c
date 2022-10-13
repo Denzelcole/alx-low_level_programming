f49bd76782bf11550171b5da4f591c2ed7068d7a
Denzelcole <theocole4@gmail.com>
2022-10-13 15:48:02 -0700

    Adding printf

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/*
+* We use prinf this time around to print out
+* with proper grammer, but the outcome is a piece of art,
+* \n - means adding a new line
+*/
+
+int main(void);
+{
+	prinf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
