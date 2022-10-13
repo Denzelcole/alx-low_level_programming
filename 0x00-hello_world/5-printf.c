1e104f8044006e0cc4a6f53e9706dd242a9945c1
Denzelcole <theocole4@gmail.com>
2022-10-13 16:00:25 -0700

    betty wicked

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-/*
+
+/**
+* main - print the string in the printf function
+*
+* Description:
 * We use prinf this time around to print out
 * with proper grammer, but the outcome is a piece of art,
 * \n - means adding a new line
+* Return: 0 (succesfully done)
 */
 
 int main(void);
 {
-	prinf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
