caaae567c7fb18dc5f4fe6345cd3d52d327cfb7e
Denzelcole <theocole4@gmail.com>
2022-10-14 15:46:13 -0700

    add faster please

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Prints numbers.
+*
+* Description - Using printf() function.
+* Counting from 0 - 9.
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int a = 0;
+
+	while (a < 9)
+{
+	printf("%d\n", a);
+	a++;
+}
+	return (0);
+}
