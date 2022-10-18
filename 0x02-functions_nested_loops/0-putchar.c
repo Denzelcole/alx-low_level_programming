92a4e94693e22eda1cc657cddb278866e3c16888
Denzelcole <theocole4@gmail.com>
2022-10-18 13:36:03 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main = Prints _putchar
+ *
+ * Description: Using _putchar function
+ * from "main.h" we print the function _putchar to stdout
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+
+    return (0);
+}
