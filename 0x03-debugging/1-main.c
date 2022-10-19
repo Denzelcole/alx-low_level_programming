b479ac7a56a2f77b6792a74be374221847d03243
Denzelcole <theocole4@gmail.com>
2022-10-19 02:52:12 -0700

    Adding edit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,8 @@
 
 /**
  * main - causes an infinite loop
+ *
+ * Commenting out the while loop and putchar
  * Return: 0
  */
 
@@ -12,15 +14,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 /**
 * while (i < 10)
 * {
 * putchar(i);
 * }
 */
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
