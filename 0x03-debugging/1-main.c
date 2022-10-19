8628fbae72fed6f1a4a126bd90df5a6d166a3f1f
Onyeoma <80336244+Denzelcole@users.noreply.github.com>
2022-10-19 21:09:07 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
-  * main - Check and prints if lastdigit is positive or negative.
-  *
-  * Description: Using the srand(), rand() function.
-  * This check if the lastdigit is greater than, equal or
-  * not equal a given number.
-  *
-  * Return: 0 (Success)
-  */
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
 int main(void)
 {
-	int n;
-	int lastdigit;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
-	if (lastdigit > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, lastdigit);
-	else if (lastdigit == 0)
-		printf("Last digit of %d is %d and is 0", n, lastdigit);
-	else if (lastdigit < 6 && lastdigit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastdigit);
-	printf("\n");
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	* while (i < 10)
+	* {
+	*	putchar(i);
+	* }
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
