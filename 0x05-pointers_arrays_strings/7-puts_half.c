50a409a6c135b15e763b0b677d6869cca1f80b48
Denzelcole <theocole4@gmail.com>
2022-10-24 14:14:04 -0700

    Adding a function that prints last half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-*puts_half - print the last half of a string
+* puts_half - print the last half of a string
+* @str: string to be processed
 *
 * Decription: if the string length is odd print thje last (length - 1) / 2
 *
 * Return: void (nothing)
 */
-void puts_half(char -str)
+void puts_half(char *str)
 {
 	int i, j;
 
