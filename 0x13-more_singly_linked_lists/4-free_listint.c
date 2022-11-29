39742f4549016a3da3b4b43031e121ca6fc1fa14
Denzelcole <theocole4@gmail.com>
2022-11-29 13:16:27 -0800

    commiting again

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,12 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * free_listint - Frees a list_t list
+ *
  * @head: Pointer to the head/first node of list_t
+ *
  */
-
 void free_listint(listint_t *head)
 {
 	listint_t *placeholder;
