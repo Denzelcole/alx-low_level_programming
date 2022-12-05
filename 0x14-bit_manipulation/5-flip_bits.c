466944d2ed66f89c6797a8d9cd61f6545cd62bc5
Denzelcole <theocole4@gmail.com>
2022-12-05 13:10:28 -0800

    nawa

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip
- * to get from one number to another.
- * @n: The number.
- * @m: The number two.
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
+ * @n: number one.
+ * @m: number two.
  *
  * Return: number of bits.
  */
@@ -14,8 +14,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
-		if ((n & 1) != (m & 1));
+		if ((n & 1) != (m & 1))
 			nbits++;
 	}
+
 	return (nbits);
 }
