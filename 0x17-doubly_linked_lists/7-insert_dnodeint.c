ac119e03b6643a574fcb29a0d8a4ac4e651c4b01
Denzelcole <theocole4@gmail.com>
2023-01-15 15:20:27 -0800

    6 is wrong

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,49 +2,41 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert a node at a given index
- *
- * @h: pointer to the head node
- * @idx: index to insert at
- * @num: integer data of the new node
- *
- * Return: address of the new node, or NULL if it fails
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int num)
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: pointer to head of doubly linked list
+* @idx: index of the list where new node should be added
+* @n: value of new node
+* Return: address of new node, or NULL if failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *previous, *newnode;
-	unsigned int counter = 0;
+    dlistint_t *new, *temp = *h;
+    unsigned int i;
 
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
-		return (NULL);
-	newnode->n = num;
-	current = *h;
-	previous = NULL;
-	do {
-		if (idx == counter)
-		{
-			if (previous)
-			{
-				previous->next = newnode;
-				newnode->prev = previous;
-			}
-			else
-			{
-				*h = newnode;
-			}
-			newnode->next = current;
-			if (current)
-				current->prev = newnode;
-			return (newnode);
-		}
-		counter++;
-		previous = current;
-		if (current)
-			current = current->next;
-	} while (previous);
-
-	free(newnode);
-
-	return (NULL);
+    new = malloc(sizeof(dlistint_t));
+    if (!new)
+        return (NULL);
+    new->n = n;
+    if (idx == 0)
+    {
+        new->prev = NULL;
+        new->next = *h;
+        if (*h)
+            (*h)->prev = new;
+        *h = new;
+        return (new);
+    }
+    for (i = 0; temp && i < idx - 1; i++)
+        temp = temp->next;
+    if (!temp)
+    {
+        free(new);
+        return (NULL);
+    }
+    new->prev = temp;
+    new->next = temp->next;
+    if (temp->next)
+        temp->next->prev = new;
+    temp->next = new;
+    return (new);
 }
