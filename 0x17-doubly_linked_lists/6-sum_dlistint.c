2e03d43b7f061affa2f7a38cff575caafbe86566
Denzelcole <theocole4@gmail.com>
2023-01-15 15:19:30 -0800

    retrying

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,42 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * sum_dlistint - compute the sum of integer data in a doubly linked list
- *
- * @head: the head node
- *
- * Return: sum of the numbers, or 0 if list is empty
- */
-int sum_dlistint(dlistint_t *head)
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: pointer to head of doubly linked list
+* @idx: index of the list where new node should be added
+* @n: value of new node
+* Return: address of new node, or NULL if failed
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	int sum = 0;
+    dlistint_t *new, *temp = *h;
+    unsigned int i;
 
-	while (head)
-	{
-		sum += head->n;
-		head = head->next;
-	}
-
-	return (sum);
+    new = malloc(sizeof(dlistint_t));
+    if (!new)
+        return (NULL);
+    new->n = n;
+    if (idx == 0)
+    {
+        new->prev = NULL;
+        new->next = *h;
+        if (*h)
+            (*h)->prev = new;
+        *h = new;
+        return (new);
+    }
+    for (i = 0; temp && i < idx - 1; i++)
+        temp = temp->next;
+    if (!temp)
+    {
+        free(new);
+        return (NULL);
+    }
+    new->prev = temp;
+    new->next = temp->next;
+    if (temp->next)
+        temp->next->prev = new;
+    temp->next = new;
+    return (new);
 }
